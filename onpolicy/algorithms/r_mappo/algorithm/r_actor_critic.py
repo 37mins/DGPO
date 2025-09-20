fix overflow bugs

onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
@@ -135,7 +135,9 @@ class R_Discriminator(nn.Module):
                         action
                     )
 
-                action0_prob = torch.exp(torch.sum(action0_log_prob, dim=-1))
+                action0_log_prob_sum = torch.sum(action0_log_prob, dim=-1)
+                action0_log_prob_sum = torch.clamp(action0_log_prob_sum, max=50, min=50)
+                action0_prob = torch.exp(action0_log_prob_sum)
 
                 batch_size = action.shape[0]
                 max_z = self.max_z
@@ -151,8 +153,11 @@ class R_Discriminator(nn.Module):
                             actor_features,
                             z,
                         )
-                    action_prob = torch.exp(torch.sum(action_log_prob, dim=-1))
+                    action_log_prob_sum = torch.sum(action_log_prob, dim=-1)
+                    action_log_prob_sum = torch.clamp(action_log_prob_sum, max=50, min=-50)
+                    action_prob = torch.exp(action_log_prob_sum)
                     conditioned_prob = action0_prob / (action0_prob + action_prob)
+                    conditioned_prob = torch.clamp(conditioned_prob, min=1e-8,max=1)
                     return torch.log(conditioned_prob)
 
                 import itertools

onpolicy/envs/mujoco/MuJoCo_env.py
@@ -45,7 +45,7 @@ class MuJocoEnv:
 ###
 
     def step(self,a):
-        k=0.04
+        k=0.011511
 
         a = a[0]
         ret = self.env.step(a)
@@ -58,7 +58,7 @@ class MuJocoEnv:
         # print(qpos.shape, qvel.shape)
         obs = [np.concatenate([qpos, qvel])]
         r4c = reward[0][0]
-        r4c = 1/(1+np.exp(-k*r4c))
+        r4c = (np.tanh(k*r4c)+1)/2
 
         if self.classifier.use_classifier:
             oracle = self.classifier.model(obs[0])

onpolicy/envs/mujoco/VMAPD_wrapper.py
@@ -39,7 +39,7 @@ class VMAPDWrapper(object):
         elif self.fix_z is not None:
             self.cur_z = self.fix_z
         else:
-            self.cur_z = np.random.randint(self.max_z) 
+            self.cur_z = np.random.randint(self.max_z)
         
         obs_n = self.env.reset()
         